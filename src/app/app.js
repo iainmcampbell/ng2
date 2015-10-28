3a59a221c974a9b8c3a4b8fca969a784e23dac7c
iainmcampbell <hey@iainmc.ca>
2015-10-28 17:14:33 -0400

    update

--- src/app/app.js
+++ src/app/app.js
@@ -13,16 +13,44 @@ var angular2_1 = require('angular2/angular2');
 var AppComponent = (function () {
     function AppComponent() {
         this.title = 'Roster of Cats';
-        this.cat = 'Grumpy';
+        this.cats = CATS;
+        this.cat = {
+            id: 1,
+            name: 'Grumpy'
+        };
     }
+    AppComponent.prototype.onSelect = function (cat) { this.selectedCat = cat; };
+    AppComponent.prototype.getSelectedClass = function (cat) { return { 'mod-selected': cat === this.selectedCat }; };
     AppComponent = __decorate([
         angular2_1.Component({
             selector: 'app',
-            template: '<h1>{{title}}</h1><h2>{{cat}}</h2>'
+            template: "\n    <h1>{{title}}</h1>\n\n    <ul class=\"cats\">\n      <li *ng-for=\"#cat of cats\"\n          class=\"cats-cat\"\n          (click)=\"onSelect(cat)\"\n          [ng-class]=\"getSelectedClass(cat)\">\n        <span class=\"cats-cat-badge\">{{cat.id}}</span> {{ cat.name }}\n      </li>\n    </ul>\n\n    <div *ng-if=\"selectedCat\">\n      <h3>Your cat: {{ selectedCat.name }}</h3>\n      Edit name: <input [(ng-model)]=\"selectedCat.name\" placeholder=\"name\" type=\"text\" />\n    </div>\n  ",
+            styles: ["\n    .cats {\n      list-style-type: none;\n      margin: 0;\n      padding: 0;\n    }\n    .cats-cat {\n      display: inline-block;\n      background: #eee;\n      margin-bottom: 0.5em;\n      margin-right: 0.5em;\n      padding-right: 0.3em;\n      cursor: pointer;\n    }\n    .cats-cat.mod-selected {\n      background: #000;\n      color: white;\n    }\n    .cats-cat-badge {\n      display: inline-block;\n      background: grey;\n      color: white;\n      padding: 0.2em 0.3em;\n    }\n  "],
+            directives: [angular2_1.FORM_DIRECTIVES, angular2_1.CORE_DIRECTIVES]
         }), 
         __metadata('design:paramtypes', [])
     ], AppComponent);
     return AppComponent;
 })();
+;
+var Cat = (function () {
+    function Cat() {
+    }
+    return Cat;
+})();
+;
+var CATS = [
+    { "id": 10, "name": "Grumpy" },
+    { "id": 11, "name": "Mr. Nice" },
+    { "id": 12, "name": "Narco" },
+    { "id": 13, "name": "Bombasto" },
+    { "id": 14, "name": "Celeritas" },
+    { "id": 15, "name": "Magneta" },
+    { "id": 16, "name": "RubberMan" },
+    { "id": 17, "name": "Dynama" },
+    { "id": 18, "name": "Dr IQ" },
+    { "id": 19, "name": "Magma" },
+    { "id": 20, "name": "Tornado" }
+];
 angular2_1.bootstrap(AppComponent);
 //# sourceMappingURL=app.js.map
--- src/app/app.ts
+++ src/app/app.ts
@@ -1,14 +1,84 @@
-import {Component, bootstrap} from 'angular2/angular2';
+import {Component, bootstrap, FORM_DIRECTIVES, CORE_DIRECTIVES } from 'angular2/angular2';
 
 @Component({
-    selector: 'app',
-    template: '<h1>{{title}}</h1><h2>{{cat}}</h2>'
+  selector: 'app',
+  template: `
+    <h1>{{title}}</h1>
+
+    <ul class="cats">
+      <li *ng-for="#cat of cats"
+          class="cats-cat"
+          (click)="onSelect(cat)"
+          [ng-class]="getSelectedClass(cat)">
+        <span class="cats-cat-badge">{{cat.id}}</span> {{ cat.name }}
+      </li>
+    </ul>
+
+    <div *ng-if="selectedCat">
+      <h3>Your cat: {{ selectedCat.name }}</h3>
+      Edit name: <input [(ng-model)]="selectedCat.name" placeholder="name" type="text" />
+    </div>
+  `,
+  styles:[`
+    .cats {
+      list-style-type: none;
+      margin: 0;
+      padding: 0;
+    }
+    .cats-cat {
+      display: inline-block;
+      background: #eee;
+      margin-bottom: 0.5em;
+      margin-right: 0.5em;
+      padding-right: 0.3em;
+      cursor: pointer;
+    }
+    .cats-cat.mod-selected {
+      background: #000;
+      color: white;
+    }
+    .cats-cat-badge {
+      display: inline-block;
+      background: grey;
+      color: white;
+      padding: 0.2em 0.3em;
+    }
+  `],
+  directives: [ FORM_DIRECTIVES, CORE_DIRECTIVES  ]
 })
 
 class AppComponent {
-
   public title = 'Roster of Cats';
-  public cat = 'Grumpy';
-}
+  public cats = CATS;
+  public cat = {
+    id: 1,
+    name: 'Grumpy'
+  };
+
+  public selectedCat: Cat;
+
+  onSelect(cat: Cat){ this.selectedCat = cat; }
+
+  getSelectedClass(cat: Cat){ return { 'mod-selected': cat === this.selectedCat } }
+};
+
+class Cat {
+  id: number;
+  name: string;
+};
+
+var CATS: Cat[] = [
+  { "id": 10, "name": "Grumpy" },
+  { "id": 11, "name": "Mr. Nice" },
+  { "id": 12, "name": "Narco" },
+  { "id": 13, "name": "Bombasto" },
+  { "id": 14, "name": "Celeritas" },
+  { "id": 15, "name": "Magneta" },
+  { "id": 16, "name": "RubberMan" },
+  { "id": 17, "name": "Dynama" },
+  { "id": 18, "name": "Dr IQ" },
+  { "id": 19, "name": "Magma" },
+  { "id": 20, "name": "Tornado" }
+]
 
 bootstrap(AppComponent);
